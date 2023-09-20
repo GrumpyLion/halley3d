33db8d4bee24a370fc9b500be417cd3467b0d8b2
Lion Schitik <schitiklion@gmail.com>
2023-09-20 02:34:07 +0200

    Update flight_helmet gltf and sample with latest changes

--- src/game_stage.cpp
+++ src/game_stage.cpp
@@ -10,7 +10,7 @@ void GameStage::init()
 	world->addService(std::make_shared<ScreenService>(this));
 
 	meshRenderer = std::make_shared<MeshRenderer>();
-	meshRenderer->setMesh(getResources().get<Mesh>("flight_helmet.glb"));
+	meshRenderer->setMesh(getResources().get<Mesh>("flight_helmet.gltf"));
 }
 
 void GameStage::onFixedUpdate(Time t)
@@ -33,9 +33,9 @@ void GameStage::onRender(RenderContext& rc) const
 
 	const float c = std::cos(float(curTime));
 	const float s = std::sin(float(curTime));
-	const float r = 1.5f;
-	const Vector3f p = Vector3f(r * s, 0.0f, r * -c);
-	const Vector3f t = Vector3f(0, 0, 0);
+	const float r = 0.5f;
+	const Vector3f p = Vector3f(r * s, 0.5f, r * -c);
+	const Vector3f t = Vector3f(0, 0.5f, 0);
 	const auto q = Quaternion::lookAt(t - p, Vector3f(0, 1, 0));
 
 	Camera cam;
